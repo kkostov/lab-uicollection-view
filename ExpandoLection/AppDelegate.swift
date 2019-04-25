6a08296ec0a28c13789fee15f89ec0355ba71954
Konstantin Kostov <konstantin@headbright.be>
2019-04-25 07:50:31 +0200

    hello world

--- ExpandoLection/AppDelegate.swift
+++ ExpandoLection/AppDelegate.swift
@@ -15,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let vc = ViewController()
+        
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+        window?.makeKeyAndVisible()
+        
         // Override point for customization after application launch.
         return true
     }
