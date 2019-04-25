332f6ceef62fc02d2076b05fcb1244e49819c715
Konstantin Kostov <konstantin@headbright.be>
2019-04-25 07:47:43 +0200

    Initial Commit

---
+++ ExpandoLection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpandoLection
+//
+//  Created by Konstantin Kostov on 25/04/2019.
+//  Copyright © 2019 Konstantin Kostov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
